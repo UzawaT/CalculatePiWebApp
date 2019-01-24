72bb4c2b6923986c1dfdadf4183b9e865a1e975d
Takafumi Uzawa <tuzawa914@gmail.com>
2019-01-24 14:39:30 -0500

    initial commit
    initial commit

---
+++ CalculatePiWebApp/CalculatePiWebApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatePiWebApp.Models;
+using System.Numerics;
+
+namespace CalculatePiWebApp.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            CalculatePiModel model = new CalculatePiModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(int points) {
+            //instantiate the model object
+            CalculatePiModel model = new CalculatePiModel();
+
+            //instantiate a random object
+            Random random = new Random();
+
+            //assign number of points
+            model.NumberOfPoints = points;
+
+            //set up an array of points
+            model.Points = new Point[points];
+
+            //set properties for each points
+            for (int i = 0; i < points; i++) {
+                model.Points[i] = new Point {
+                    PointVector = new Vector2((float)random.NextDouble(), (float)random.NextDouble())
+                };
+
+                if (model.Points[i].PointVector.LengthSquared() < 1) {
+                    model.Points[i].PointColor = "red";
+                }      
+            }
+
+            //assign the values to calculated value, percent error, and points in range
+            model.PointsInRange = model.Points.Where(n => n.PointColor == "red").Count();
+            model.CalculatedValue = 4.0 * model.PointsInRange / model.NumberOfPoints;
+            model.PercentError = Math.Abs(model.CalculatedValue - Math.PI) / Math.PI * 100;
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalculatePiWebApp/CalculatePiWebApp/Models/CalculatePiModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace CalculatePiWebApp.Models {
+    public class CalculatePiModel {
+        //properties
+        public int NumberOfPoints { get; set; }
+        public int PointsInRange { get; set; }
+        public double CalculatedValue { get; set; }
+        public double PercentError { get; set; }
+        public Point[] Points { get; set; }
+        public Chart Chart { get; set; }
+
+        //constructor
+        public CalculatePiModel() {
+            NumberOfPoints = 0;
+            PointsInRange = 0;
+            CalculatedValue = 0;
+            PercentError = 0;
+            Points = new Point[NumberOfPoints];
+            Chart = new Chart(200, 200, 1);
+        }
+    }
+
+    public class Point {
+        //properties
+        public Vector2 PointVector { get; set; }
+        public string PointColor { get; set; }
+
+        //constructor
+        public Point() {
+            PointVector = new Vector2(0, 0);
+            PointColor = "blue";
+        }
+    }
+
+    public class Chart {
+        //properties
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int PointSize { get; set; }
+
+        //constructor
+        public Chart(int width, int height, int size) {
+            Width = width;
+            Height = height;
+            PointSize = size;
+        }
+    }
+}
